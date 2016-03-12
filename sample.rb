f7c8eb20cfa41fe8fa6d584a921b30d98d8b260f
katoy <youichikato@gmail.com>
2016-03-12 19:26:28 +0900

    Update sample.rb (in progress...)

--- sample.rb
+++ sample.rb
@@ -10,7 +10,7 @@ def pp(x)
   puts Hirb::Helpers::AutoTable.render eval(x)
   puts
 rescue StandardError => ex
-  puts "#{ex}"
+  puts "Exception: #{ex}"
 end
 
 =begin
@@ -48,7 +48,8 @@ pp 'q = Client.limit(3)'
 
 pp 'q = Client.limit(3).offset(3)'
 
-pp 'Order.select("date(ordered_at) as ordered_date, sum(price) as total_price").group("date(ordered_at)")'
+pp 'Order.select("date(ordered_at) as ordered_date, sum(price) as total_price").
+    group("date(ordered_at)")'
 
 pp 'Order.select("date(ordered_at) as ordered_date, sum(price) as total_price").
     group("date(ordered_at)").having("sum(price) > ?", 400)'
@@ -65,24 +66,106 @@ pp 'Client.none'
 =end
 
 # 1. データベースからオブジェクトを取り出す
-# 2. 条件
-# 3. 並び順
-# 4. 特定のフィールドだけを取り出す
-# 5. LimitとOffset
-# 6. グループ
-# 7. Having
-# 8. 条件を上書きする
-# 9. Nullリレーション
-# 10. 読み取り専用オブジェクト
-# 11. レコードを更新できないようロックする
-# 12. テーブルを結合する
-# 13. 関連付けを一括読み込みする
-# 14. スコープ
-# 15. 動的ファインダ
-# 16. メソッドチェインを理解する
+# ---------------------------------------------------
+# =begin
+pp 'Client.find(1)'
+pp 'Client.find([1, 5, 3])'
+pp 'Client.take'
+pp 'Client.take 3'
+pp 'Client.first'
+pp 'Client.first(3)'
+pp 'Client.last'
+pp 'Client.last(3)'
 
+pp 'Client.find_by first_name: "安倍"'
+pp 'Client.find_by first_name: "no-exist"'
+pp 'Client.find_by! first_name: "no-exist"'
+
+pp '
+  Client.find_each do |cient|
+    puts client.first_name
+  end
+'
+pp '
+  Client.find_each(batch_size: 3) do |client|
+    puts client.first_name
+  end
+'
+pp '
+  Client.find_in_batches do |clients|
+    clients.each do |c| puts c.first_name end
+  end
+'
+pp '
+  Client.find_in_batches(batch_size: 3) do |clients|
+    clients.each do |c| puts c.first_name end
+  end
+'
+# =end
+
+# 2. 条件
+# ---------------------------------------------------
 =begin
+=end
+# 3. 並び順
+# ---------------------------------------------------
+=begin
+=end
+# 4. 特定のフィールドだけを取り出す
+# ---------------------------------------------------
+=begin
+=end
+# 5. LimitとOffset
+# ---------------------------------------------------
+=begin
+=end
+# 6. グループ
+# ---------------------------------------------------
+=begin
+=end
+# 7. Having
+# ---------------------------------------------------
+=begin
+=end
+# 8. 条件を上書きする
+# ---------------------------------------------------
+=begin
+=end
+# 9. Nullリレーション
+# ---------------------------------------------------
+=begin
+=end
+# 10. 読み取り専用オブジェクト
+# ---------------------------------------------------
+=begin
+=end
+# 11. レコードを更新できないようロックする
+# ---------------------------------------------------
+=begin
+=end
+# 12. テーブルを結合する
+# ---------------------------------------------------
+=begin
+=end
+# 13. 関連付けを一括読み込みする
+# ---------------------------------------------------
+=begin
+=end
+# 14. スコープ
+# ---------------------------------------------------
+=begin
+=end
+# 15. 動的ファインダ
+# ---------------------------------------------------
+=begin
+=end
+# 16. メソッドチェインを理解する
+# ---------------------------------------------------
+=begin
+=end
 # 17. 新しいオブジェクトを検索またはビルドする
+# ---------------------------------------------------
+=begin
 pp 'Client.find_or_create_by(first_name: "Andy", last_name: "Smith")'
 
 pp 'Client.create_with(locked: true).find_or_create_by(first_name: "AndyX")'
@@ -102,8 +185,10 @@ nick.save!
 p "after save:"
 p nick
 '
-
+=end
 # 18. SQLで検索する
+# ---------------------------------------------------
+=begin
 pp 'Client.where(locked: true).pluck(:id)'
 
 pp 'Client.select(:id, :first_name).map { |c| [c.id, c.first_name] }'
@@ -113,16 +198,21 @@ pp 'Client.limit(2).pluck(:id, :first_name, :last_name)'
 pp 'Client.ids'
 
 # 19. オブジェクトの存在チェック
+# ---------------------------------------------------
 pp 'Client.exists?(1)'
 pp 'Client.exists?(id: [1,2,99])'
 pp 'Client.where(first_name: "Ryan").exists?'
 pp 'Client.exists?'
 pp 'Client.all.first.orders.any?'
+#end
 
 # 20. 計算
+# ---------------------------------------------------
+#begin
 pp 'Client.count'
 pp 'Client.where(first_name: "安倍").count'
-pp 'Client.includes("orders").where(first_name: "Ryan", orders: { status: :received }).count'
+pp 'Client.includes("orders").where(first_name: "Ryan",
+                                    orders: { status: :received }).count'
 pp 'Client.count(:orders_count)'
 
 pp 'Client.average("orders_count")'
@@ -130,5 +220,5 @@ pp 'Client.minimum("orders_count")'
 pp 'Client.maximum("orders_count")'
 
 # 21. EXPLAINを実行する
-
+# ---------------------------------------------------
 =end
